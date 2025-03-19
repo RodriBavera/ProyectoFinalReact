f7c5bba248d472dfe316e95376da579c8f950547
Rodrigo <rodri.crzz@gmail.com>
2025-03-18 23:33:56 -0300

    Proyecto Final Listo 2

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -21,21 +21,21 @@ const ItemDetail = ({ id, nombre, precio, img, descripcion, stock }) => {
     }
 
     return (
-        <div className="item-detail-container">
-            <h2>Detalles del producto</h2>
-            <div className="item-detail-card">
-                <img src={img} alt={nombre} />
-                <h3>Nombre: {nombre}</h3>
-                <h3>Precio: {precio}</h3>
-                <p><strong>Descripción:</strong> {descripcion}</p>
-                {
-                    agregarCantida > 0 ? (<Link to="/cart" className='btn-terminar'> Terminar Compra</Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />)
-                }
-                <Link to="/" className="btn">
-                    Volver a la tienda
-                </Link>
-
-
+        <div className="detail-page-container">
+            <h2 className='detalleProducto'>Detalles del producto</h2>
+            <div className="item-detail-container">
+                <div className="item-detail-card">
+                    <img src={img} alt={nombre} />
+                    <h3>Nombre: {nombre}</h3>
+                    <h3>Precio: {precio}</h3>
+                    <p><strong>Descripción:</strong> {descripcion}</p>
+                    {
+                        agregarCantida > 0 ? (<Link to="/cart" className='btn-terminar'> Terminar Compra</Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />)
+                    }
+                    <Link to="/" className="btn">
+                        Volver a la tienda
+                    </Link>
+                </div>
             </div>
         </div>
     )
